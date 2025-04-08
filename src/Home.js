cde44282fdba4784f0e8681473d83714a71654a4
Kartheepan-M <114690092+Kartheepan-M@users.noreply.github.com>
2025-04-08 19:07:04 +0530

    loader animation added

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,109 @@
+// import React, { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import axios from "axios";
+// import "./Home.css"; // Updated CSS
+
+// function Home() {
+//   const [image, setImage] = useState(null);
+//   const [previewUrl, setPreviewUrl] = useState(null);
+//   const [result, setResult] = useState(null);
+//   const [loading, setLoading] = useState(false);
+//   const navigate = useNavigate();
+
+//   const handleImageChange = (event) => {
+//     const file = event.target.files[0];
+//     setImage(file);
+//     setPreviewUrl(URL.createObjectURL(file)); // Show preview
+//   };
+
+//   const analyze_service = async () => {
+//     if (!image) {
+//       alert("Please select an image first!");
+//       return;
+//     }
+
+//     const formData = new FormData();
+//     formData.append("image", image);
+//     setLoading(true);
+//     try {
+//       const response = await axios.post("http://localhost:5000/analyze", formData, {
+//         headers: { "Content-Type": "multipart/form-data" },
+//       });
+
+//       setResult(response.data);
+//     } catch (error) {
+//       console.error("Error analyzing image:", error);
+//       alert("Failed to analyze the image.");
+//     }
+//     finally{
+//       setLoading(false);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       {/* Navbar */}
+//       <nav className="navbar">
+//         <div className="navbar-title">Skin Lesion Analysis</div>
+//         <ul className="nav-links">
+//           <li onClick={() => navigate("/")}>Home</li>
+//           <li onClick={() => navigate("/report", { state: result })}>Report</li>
+//           <li onClick={() => navigate("/about")}>About</li>
+//         </ul>
+//       </nav>
+
+//       {/* Upload Section */}
+//       <div className="upload-section">
+//         <h1>Upload an Image for Analysis</h1>
+
+//         <label htmlFor="file-upload" className="file-label" >Choose Image</label><br></br>
+//         <input
+//           id="file-upload"
+//           type="file"
+//           className="file-input"
+//           onChange={handleImageChange}
+//           accept="image/*"
+//         />
+
+//         {previewUrl && (
+//           <div className="preview-container">
+//             <img src={previewUrl} alt="Preview" className="preview-image" />
+//           </div>
+//         )}
+//         <button className="analyze-button" onClick={analyze_service} disabled={loading}>Analyze</button>
+
+//         {/* Display Results */}
+//         {result && (
+//           <div >
+//             <h2>Analysis Result:</h2>
+//             <p><b>Class Name:</b> {result.class_name}</p>
+//             {/*<p><b>Additional Info:</b> {result.additional_info}</p>*/}
+//             <button className="report-button" onClick={() => navigate("/report", { state: result })}>
+//               View Full Report
+//             </button>
+//           </div>
+//         )}
+
+//       </div>
+
+//       {/* Display Results 
+//       {result && (
+//         <div className="result-container">
+//           <h2>Analysis Result:</h2>
+//           <img src={result.image_url} alt="Analyzed" className="result-image" />
+//           <p><b>Class Name:</b> {result.class_name}</p>
+//           <button className="report-button" onClick={() => navigate("/report", { state: result })}>
+//             View Full Report
+//           </button>
+//         </div>
+//       )}
+//       */}
+      
+//     </div>
+//   );
+// }
+
+// export default Home;
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -7,6 +113,7 @@ function Home() {
   const [image, setImage] = useState(null);
   const [previewUrl, setPreviewUrl] = useState(null);
   const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   const handleImageChange = (event) => {
@@ -15,7 +122,7 @@ function Home() {
     setPreviewUrl(URL.createObjectURL(file)); // Show preview
   };
 
-  const handleUpload = async () => {
+  const analyze_service = async () => {
     if (!image) {
       alert("Please select an image first!");
       return;
@@ -23,7 +130,7 @@ function Home() {
 
     const formData = new FormData();
     formData.append("image", image);
-
+    setLoading(true);
     try {
       const response = await axios.post("http://localhost:5000/analyze", formData, {
         headers: { "Content-Type": "multipart/form-data" },
@@ -33,11 +140,13 @@ function Home() {
     } catch (error) {
       console.error("Error analyzing image:", error);
       alert("Failed to analyze the image.");
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <div>
+    <div className="home-container">
       {/* Navbar */}
       <nav className="navbar">
         <div className="navbar-title">Skin Lesion Analysis</div>
@@ -52,7 +161,10 @@ function Home() {
       <div className="upload-section">
         <h1>Upload an Image for Analysis</h1>
 
-        <label htmlFor="file-upload" className="file-label">Choose Image</label>
+        <label htmlFor="file-upload" className="file-label">
+          Choose Image
+        </label>
+        <br />
         <input
           id="file-upload"
           type="file"
@@ -66,36 +178,27 @@ function Home() {
             <img src={previewUrl} alt="Preview" className="preview-image" />
           </div>
         )}
-
-        <button className="analyze-button" onClick={handleUpload}>Analyze</button>
+        <button className="analyze-button" onClick={analyze_service} disabled={loading}>
+          {loading ? <div className="loader"><span>.</span><span>.</span><span>.</span></div> : "Analyze"}
+        </button>
 
         {/* Display Results */}
         {result && (
-          <div >
+          <div className="result-container">
             <h2>Analysis Result:</h2>
-            <p><b>Class Name:</b> {result.class_name}</p>
+            <p>
+              <b>Class Name:</b> {result.class_name}
+            </p>
             {/*<p><b>Additional Info:</b> {result.additional_info}</p>*/}
-            <button className="report-button" onClick={() => navigate("/report", { state: result })}>
+            <button
+              className="report-button"
+              onClick={() => navigate("/report", { state: result })}
+            >
               View Full Report
             </button>
           </div>
         )}
-
       </div>
-
-      {/* Display Results 
-      {result && (
-        <div className="result-container">
-          <h2>Analysis Result:</h2>
-          <img src={result.image_url} alt="Analyzed" className="result-image" />
-          <p><b>Class Name:</b> {result.class_name}</p>
-          <button className="report-button" onClick={() => navigate("/report", { state: result })}>
-            View Full Report
-          </button>
-        </div>
-      )}
-      */}
-      
     </div>
   );
 }
